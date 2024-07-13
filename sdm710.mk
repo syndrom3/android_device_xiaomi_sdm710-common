79583ac873bf782af6b13937c7019d673ed2990a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    sdm710-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- sdm710.mk
+++ sdm710.mk
@@ -134,9 +134,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm710
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.sdm710
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
